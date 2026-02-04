628874b1d4fe1096a3eb528aa466a7716fca4755
Mathias Oertel <mathias.oertel@commercetools.com>
2026-02-04 22:56:56 +0100

    nvim: add nvim-dap for debugging (rust, go, typescript)

--- dot_config/nvim/lua/usr/plugins.lua
+++ dot_config/nvim/lua/usr/plugins.lua
@@ -47,6 +47,7 @@ require("lazy").setup({
         },
       })
       wk.add({
+        { "<leader>d", group = "debug" },
         { "<leader>f", group = "find" },
         { "<leader>g", group = "git" },
         { "<leader>h", group = "harpoon" },
@@ -597,10 +598,129 @@ require("lazy").setup({
   },
   { "Maan2003/lsp_lines.nvim", event = "LspAttach" },
 
+  -- Debugging (DAP)
+  {
+    "mfussenegger/nvim-dap",
+    keys = {
+      { "<leader>db", function() require("dap").toggle_breakpoint() end, desc = "Toggle breakpoint" },
+      { "<leader>dB", function() require("dap").set_breakpoint(vim.fn.input("Condition: ")) end, desc = "Conditional breakpoint" },
+      { "<leader>dc", function() require("dap").continue() end, desc = "Continue" },
+      { "<leader>di", function() require("dap").step_into() end, desc = "Step into" },
+      { "<leader>do", function() require("dap").step_over() end, desc = "Step over" },
+      { "<leader>dO", function() require("dap").step_out() end, desc = "Step out" },
+      { "<leader>dr", function() require("dap").repl.open() end, desc = "Open REPL" },
+      { "<leader>dl", function() require("dap").run_last() end, desc = "Run last" },
+      { "<leader>dt", function() require("dap").terminate() end, desc = "Terminate" },
+    },
+    dependencies = {
+      -- UI for DAP
+      {
+        "rcarriga/nvim-dap-ui",
+        dependencies = { "nvim-neotest/nvim-nio" },
+        config = function()
+          local dap, dapui = require("dap"), require("dapui")
+          dapui.setup()
+          dap.listeners.after.event_initialized["dapui_config"] = function() dapui.open() end
+          dap.listeners.before.event_terminated["dapui_config"] = function() dapui.close() end
+          dap.listeners.before.event_exited["dapui_config"] = function() dapui.close() end
+        end,
+        keys = {
+          { "<leader>du", function() require("dapui").toggle() end, desc = "Toggle DAP UI" },
+          { "<leader>de", function() require("dapui").eval() end, desc = "Eval", mode = { "n", "v" } },
+        },
+      },
+      -- Virtual text for variables
+      {
+        "theHamsta/nvim-dap-virtual-text",
+        config = function()
+          require("nvim-dap-virtual-text").setup()
+        end,
+      },
+      -- Mason integration
+      {
+        "jay-babu/mason-nvim-dap.nvim",
+        dependencies = { "williamboman/mason.nvim" },
+        config = function()
+          require("mason-nvim-dap").setup({
+            ensure_installed = { "codelldb", "delve", "js" },
+            automatic_installation = true,
+          })
+        end,
+      },
+    },
+    config = function()
+      local dap = require("dap")
+
+      -- Go (delve)
+      dap.adapters.delve = {
+        type = "server",
+        port = "${port}",
+        executable = {
+          command = "dlv",
+          args = { "dap", "-l", "127.0.0.1:${port}" },
+        },
+      }
+      dap.configurations.go = {
+        {
+          type = "delve",
+          name = "Debug",
+          request = "launch",
+          program = "${file}",
+        },
+        {
+          type = "delve",
+          name = "Debug Package",
+          request = "launch",
+          program = "${fileDirname}",
+        },
+        {
+          type = "delve",
+          name = "Debug Test",
+          request = "launch",
+          mode = "test",
+          program = "${file}",
+        },
+      }
+
+      -- TypeScript/JavaScript (js-debug)
+      dap.adapters["pwa-node"] = {
+        type = "server",
+        host = "localhost",
+        port = "${port}",
+        executable = {
+          command = "node",
+          args = {
+            vim.fn.stdpath("data") .. "/mason/packages/js-debug-adapter/js-debug/src/dapDebugServer.js",
+            "${port}",
+          },
+        },
+      }
+      dap.configurations.typescript = {
+        {
+          type = "pwa-node",
+          request = "launch",
+          name = "Launch file",
+          program = "${file}",
+          cwd = "${workspaceFolder}",
+        },
+      }
+      dap.configurations.javascript = dap.configurations.typescript
+
+      -- Rust (codelldb) - handled by rustaceanvim
+    end,
+  },
+
   -- Rust
   {
     "mrcjkb/rustaceanvim",
     ft = { "rust" },
+    config = function()
+      vim.g.rustaceanvim = {
+        dap = {
+          autoload_configurations = true,
+        },
+      }
+    end,
   },
 
   -- Scala
