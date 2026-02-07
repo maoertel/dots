5f4f51618443483043bf70e52f4ee1b5bf56d527
Mathias Oertel <mathias.oertel@commercetools.com>
2026-02-07 18:42:28 +0100

    nvim: remove dead Format command from jsonls config
    range_formatting() was removed in Neovim 0.10, and the setup.commands
    field isn't valid in vim.lsp.config anyway.

--- dot_config/nvim/lua/usr/lsp/jsonls.lua
+++ dot_config/nvim/lua/usr/lsp/jsonls.lua
@@ -133,13 +133,4 @@ vim.lsp.config('jsonls', {
       schemas = schemas,
     },
   },
-  setup = {
-    commands = {
-      Format = {
-        function()
-          vim.lsp.buf.range_formatting({}, { 0, 0 }, { vim.fn.line "$", 0 })
-        end,
-      },
-    },
-  },
 })
