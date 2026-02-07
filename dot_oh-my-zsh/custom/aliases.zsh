ec3ae811bbe8de9c28129fc10e5d2e16e5d3db0a
Mathias Oertel <mathias.oertel@commercetools.com>
2026-02-07 13:32:06 +0100

    Clean up custom zsh aliases
    Remove unused aliases (vl, magic, boot, pd, sbtDebug), fix tf to use
    PATH-based terraform, add missing c=claude alias, and reorganize groups.

--- dot_oh-my-zsh/custom/aliases.zsh
+++ dot_oh-my-zsh/custom/aliases.zsh
@@ -1,32 +1,21 @@
 alias l="ls -FlaG"
 
 alias v="nvim"
+alias c="claude"
 
+# k8s
 alias k="kubectl"
-complete -F __start_kubectl k
 alias kns="kubens"
 alias kcx="kubectx"
 alias kon="kubeon"
 alias kof="kubeoff"
 
-alias sbtDebug="DEBUG_REVOLVER=true sbt"
+# terraform
+alias tf="terraform"
 
 # brew
 alias buu="brew update && brew upgrade"
 
 # vault
-alias vl="~/Documents/commercetools/vault/vl.sh"
-
-alias magic="sed -E '1s/^/[/; $ ! s/(^.+$)/\1,/; $ s/$/]/' | jq"
-
-# vault login
 alias vs='export VAULT_ADDR=https://vault.sre.europe-west1.gcp.commercetools.com && vault login -method=oidc -path=oidc/gsuite'
 alias vp='export VAULT_ADDR=https://vault.services.europe-west1.gcp.commercetools.com && vault login -method=oidc -path=oidc/gsuite'
-
-# kubernetes
-alias boot='k port-forward svc/bootstrap-ws 4001:8080'
-
-# get projects distribution
-alias pd="curl -s http://localhost:4001/clusters | jq '.db' | jq '.[] | to_entries | map(select(.value.projects != []) | del(.value.server)) | map({(.key): .value.projects})' | jq -s 'add | add'"
-
-alias tf="~/bin/terraform"
