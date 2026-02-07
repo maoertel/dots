f8a40a1186ab651a240092d0c5a2c21f9791f910
Mathias Oertel <mathias.oertel@commercetools.com>
2026-02-07 18:43:08 +0100

    nvim: fix async format-on-save in terraformls
    Same race condition as autocmds.lua — async=true in BufWritePre
    means the file may save before formatting completes.

--- dot_config/nvim/lua/usr/lsp/terraformls.lua
+++ dot_config/nvim/lua/usr/lsp/terraformls.lua
@@ -11,7 +11,7 @@ vim.api.nvim_create_autocmd(
   "BufWritePre", {
     pattern = { "*.tf", "*.tfvars" },
     callback = function()
-      vim.lsp.buf.format { async = true }
+      vim.lsp.buf.format { async = false }
     end
   })
 
