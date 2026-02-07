43658205b2f9cf2af734a247e8d2e610a079efbf
Mathias Oertel <mathias.oertel@commercetools.com>
2026-02-07 18:23:43 +0100

    nvim: fix async format-on-save race condition
    Using async=true in BufWritePre meant the file could save before
    formatting completed. Changed to synchronous formatting.

--- dot_config/nvim/lua/usr/lsp/autocmds.lua
+++ dot_config/nvim/lua/usr/lsp/autocmds.lua
@@ -3,6 +3,6 @@ vim.api.nvim_create_autocmd(
   "BufWritePre", {
   pattern = { "*.rs", "*.scala", "*.go" },
   callback = function()
-    vim.lsp.buf.format { async = true }
+    vim.lsp.buf.format { async = false }
   end
 })
