f41dc7f99752b6874f425034d41ab55aa15077d0
Mathias Oertel <mathias.oertel@commercetools.com>
2026-04-04 22:26:34 +0200

    fix: Disable rainbow-delimiters for NvimTree and update codex config

--- dot_config/nvim/lua/usr/plugins/rainbow.lua
+++ dot_config/nvim/lua/usr/plugins/rainbow.lua
@@ -4,6 +4,7 @@ return {
     event = "BufReadPost",
     config = function()
       local rainbow = require("rainbow-delimiters")
+      local disabled_ft = { NvimTree = true }
       vim.g.rainbow_delimiters = {
         strategy = {
           [""] = rainbow.strategy["global"],
@@ -11,6 +12,9 @@ return {
         query = {
           [""] = "rainbow-delimiters",
         },
+        condition = function(bufnr)
+          return not disabled_ft[vim.bo[bufnr].ft]
+        end,
         highlight = {
           "RainbowDelimiterYellow",
           "RainbowDelimiterBlue",
